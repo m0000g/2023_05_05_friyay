8d3f1e576efd3b400cd216c0b7e650368c0caf9c
MG <m0000g@gmail.com>
2023-05-11 15:03:29 -0600

    Data to map, mapping each card of the slider

---
+++ js/slider_list.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: 0,
+    image: '/img/card_img_placeholder.png',
+    title: 'Search form',
+    caption: 'Search from content and more',
+  },
+  {
+    id: 1,
+    image: '/img/card_img_placeholder.png',
+    title: 'Inside app',
+    caption: 'Explore our career opportunities!',
+  },
+];
